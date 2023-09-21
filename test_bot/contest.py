cd7fd6ae1ec2220852f7cd43877c3dc3d94156ad
Olga <53147140+helloole@users.noreply.github.com>
2023-09-21 23:19:44 +0500

    Update contest.py

--- test_bot/contest.py
+++ test_bot/contest.py
@@ -1,5 +1,1 @@
-bot_token = "6445391460:AAGx5jC-KJqTp4-ZpWDK1_wpgg1d4R9tnY0"
-admin_id = "@helloole"
-
 # Ссылка на бот t.me/SmartOil_test_bot
-# ТОКЕН 6445391460:AAGx5jC-KJqTp4-ZpWDK1_wpgg1d4R9tnY0
