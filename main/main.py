510520c5f1bd6c773e0d6cfd21bf068e93d0e11d
Olga <53147140+helloole@users.noreply.github.com>
2023-09-27 16:48:30 +0500

    Delete main directory

--- main/main.py
+++
@@ -1,8 +0,0 @@
-import telebot
-from constants import bot_token
-bot = telebot.TeleBot(bot_token)
-# @bot.message_handler(func=lambda message: True)
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Рады вас видеть в Маслоботе. Мы подберем масло для вашего автомобиля")
-bot.infinity_polling()
